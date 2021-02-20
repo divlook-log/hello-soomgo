e21d998f061d3b8e33dcdc76c1c3baa9b2883a9e
uihyeon.cha <uihyeon.cha@gmail.com>
2021-02-21 00:55:20 +0900

    store 수정

--- src/store/modules/form.ts
+++ src/store/modules/form.ts
@@ -1,12 +1,51 @@
-import { VuexModule, Module, getModule } from 'vuex-module-decorators'
+import {
+    VuexModule,
+    Module,
+    getModule,
+    Mutation,
+    Action,
+} from 'vuex-module-decorators'
 import store from '@/store'
+import { AnswerData, RequestForm } from '@/interfaces/form'
 
-export interface FormState {}
+export interface FormState {
+    current: RequestForm | null
+    lastAnswerData: AnswerData | null
+}
 
 @Module({
-    store,
     name: 'form',
+    store,
+    dynamic: true,
+    namespaced: true,
 })
-class FormStore extends VuexModule implements FormState {}
+export class FormStore extends VuexModule implements FormState {
+    current: RequestForm | null = null
+    lastAnswerData: AnswerData | null = null
+
+    @Mutation
+    setRequestForm(data: RequestForm | null) {
+        this.current = data
+    }
+
+    @Mutation
+    setLastAnswerData(data: AnswerData | null) {
+        this.lastAnswerData = data
+    }
+
+    @Action({ commit: 'setRequestForm' })
+    async getRequestForm(): Promise<RequestForm | null> {
+        if (this.current) {
+            return this.current
+        }
+
+        try {
+            return require('@/assets/dummy/input.json')
+        } catch (error) {
+            console.error(error)
+            return null
+        }
+    }
+}
 
 export const useFormStore = () => getModule(FormStore)
